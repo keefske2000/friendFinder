807c968d1908cc543212f4ef2a2fbcaa1ecdfe5e
keefske2000 <keith.w.collins@gmail.com>
2019-02-12 10:27:00 -0500

    update

---
+++ app/data/friends.js
@@ -0,0 +1,43 @@
+var Clients = [{
+	"name":"Bill",
+	  "photo":"assests/images/bmurray.jpg",
+	   "scores":['3','3','2','2','4','4','4','5','2','4']
+},{
+	"name":"Will",
+	  "photo":"assests/images/wsmith.jpg",
+	  "scores": ['2','2','4','5','2','2','5','3','1','2']
+},{
+  "name":"James",
+	  "photo":"assests/images/jbond.jpg",
+	  "scores": ['4','2','1','5','3','1','3','2','4','5']
+},{
+	"name":"Sean",
+		"photo":"assests/images/scarter.jpg",
+        "scores":['1','3','5','5','2','2','3','4','1','5']
+ 
+}, {"name":"Obama",
+        "photo":"assests/images/bobama.jpg",
+         "scores":['3','3','2','2','4','4','4','5','2','4']
+  },{
+      "name":"Cardi",
+        "photo":"assests/images/cardi.jpg",
+        "scores": ['2','2','4','5','2','2','5','3','1','2']
+  },{
+    "name":"Marilyn",
+        "photo":"assests/images/mmanroe.jpg",
+        "scores": ['4','2','1','5','3','1','3','2','4','5']
+  },{
+      "name":"Michelle",
+          "photo":"assests/images/mobama.jpg",
+          "scores":['1','3','5','5','2','2','3','4','1','5'] 
+  },{
+       "name":"Jenifer",
+                "photo":"assests/images/janiston.jpg",
+                "scores": ['4','2','1','5','3','1','3','2','4','5']
+  },{
+       "name":"Yoko",
+                  "photo":"assests/images/yono.jpg",
+                  "scores":['1','3','5','5','2','2','3','4','1','5']       
+}];
+
+module.exports = Clients;
